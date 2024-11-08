f9d50a4eb6cb2a6a2349f9cf89894b617f7b9b10
ocavue <ocavue@users.noreply.github.com>
2024-11-08 19:55:10 +0000

    update

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,1 +1,3 @@
-export * from "./types/opencv";
+import type { CV } from "./types/opencv";
+declare function load(): Promise<CV>;
+export default load;
--- dist/src/types/opencv/index.d.ts
+++ dist/src/types/opencv/index.d.ts
@@ -1,3 +1,3 @@
-import * as _cv from "./_types";
-export type CV = typeof _cv;
+import * as cv from "./_types";
+export type CV = typeof cv;
 export * from "./_types";
