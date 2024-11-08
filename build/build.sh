4c5eeff0546524012bdc4b3987232864cda744e8
ocavue <ocavue@users.noreply.github.com>
2024-11-08 19:00:00 +0000

    add file

--- build/build.sh
+++ build/build.sh
@@ -6,11 +6,17 @@ EMSCRIPTEN_SDK_VERSION=3.1.71
 OPENCV_VERSION=4.10.0
 
 cd $(dirname $0)
-pwd 
+ROOT_DIR=$(pwd)/..
+cd $ROOT_DIR
 
-git clone https://github.com/opencv/opencv.git
+cd "${ROOT_DIR}/build"
+git clone https://github.com/opencv/opencv.git || true
 
-cd opencv 
+cd opencv
 git checkout $OPENCV_VERSION
 
 docker run --rm -v $(pwd):/src -u $(id -u):$(id -g) emscripten/emsdk:$EMSCRIPTEN_SDK_VERSION emcmake python3 ./platforms/js/build_js.py build_js
+
+cd $ROOT_DIR
+cp build/opencv/build_js/bin/opencv.js dist/opencv-default.js
+
