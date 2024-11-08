bfb4503d93cdfede60b3a2c958cbaa26fba338ae
ocavue <ocavue@users.noreply.github.com>
2024-11-08 19:09:09 +0000

    disable_single_file

--- build/build.sh
+++ build/build.sh
@@ -10,13 +10,13 @@ ROOT_DIR=$(pwd)/..
 cd $ROOT_DIR
 
 cd "${ROOT_DIR}/build"
-git clone https://github.com/opencv/opencv.git || true
+# git clone https://github.com/opencv/opencv.git || true
 
 cd opencv
-git checkout $OPENCV_VERSION
+# git checkout $OPENCV_VERSION
 
-docker run --rm -v $(pwd):/src -u $(id -u):$(id -g) emscripten/emsdk:$EMSCRIPTEN_SDK_VERSION emcmake python3 ./platforms/js/build_js.py build_js
+docker run --rm -v $(pwd):/src -u $(id -u):$(id -g) emscripten/emsdk:$EMSCRIPTEN_SDK_VERSION emcmake python3 ./platforms/js/build_js.py build_js --disable_single_file
 
 cd $ROOT_DIR
-cp build/opencv/build_js/bin/opencv.js dist/opencv-default.js
+# cp build/opencv/build_js/bin/opencv.js dist/opencv-default.js
 
